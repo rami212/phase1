c8ddb6b372ac71c83ef34c98acbbc699883a0f01
rami212 <102681050+rami212@users.noreply.github.com>
2022-04-02 14:14:16 +0530

    Add files via upload

---
+++ EmailValidationPP.java
@@ -0,0 +1,32 @@
+package practiceproject;
+
+
+import java.util.regex.Pattern;
+
+public class EmailValidationPP {
+	
+	 public static boolean isValid(String email) {
+	 
+		 
+	 String emailRegex = "^[a-zA-Z0-9_+&-]+(?:\\.[a-zA-Z0-9_+&-]+)*@" +
+	 "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+	 Pattern pat = Pattern.compile(emailRegex);
+	 if (email == null)
+	 return false;
+	 return pat.matcher(email).matches();
+	 }
+	 public static void main(String[] args) {
+	 String email1 = "ramireddygmail.com";
+	 String email2 = "@rami.com";
+	 String email3 = "sravs@gmail.com";
+	 String[] emails= {email1,email2,email3};
+	 for (int i = 0; i < emails.length; i++) {
+	 String email=emails[i];
+	 if (isValid(email))
+	 System.out.print(email+" is valid email");
+	 else
+	 System.out.print(email+" is invalid email");
+	 System.out.println();
+	 }
+	 }
+	}
